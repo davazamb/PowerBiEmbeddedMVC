2e6b733d993d5e0b300c270a620405d338f82ee7
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-11-28 10:24:32 -0300

    Ejemplo agregando cambios

---
+++ PowerBiEmbeddedMVC/Controllers/ReportController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.PowerBI.Api.V1;
+using Microsoft.PowerBI.Security;
+using Microsoft.Rest;
+using PowerBiEmbeddedMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PowerBiEmbeddedMVC.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly string workspaceCollection;
+        private readonly string workspaceId;
+        private readonly string accessKey;
+        private readonly string apiUrl;
+
+        public ReportController()
+        {
+            this.workspaceCollection = ConfigurationManager.AppSettings["powerbi:WorkspaceCollection"];
+            this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
+            this.accessKey = ConfigurationManager.AppSettings["powerbi:AccessKey"];
+            this.apiUrl = ConfigurationManager.AppSettings["powerbi:ApiUrl"];
+        }
+
+
+
+        public async Task<ActionResult> Report(string reportId)
+        {
+            using (var client = this.CreatePowerBIClient())
+            {
+                string myUserID = User.Identity.Name.ToString();
+                IEnumerable<string> myRole = new List<string>() { "Customer", "Developer" };
+                var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, this.workspaceId);
+                var report = reportsResponse.Value.FirstOrDefault(r => r.Id == reportId);
+                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, report.Id, myUserID, myRole);
+                var viewModel = new ReportViewModel
+                {
+                    Report = report,
+                    AccessToken = embedToken.Generate(this.accessKey)
+                };
+                return View(viewModel);
+            }
+        }
+
+        private IPowerBIClient CreatePowerBIClient()
+        {
+            var credentials = new TokenCredentials(accessKey, "AppKey");
+            var client = new PowerBIClient(credentials)
+            {
+                BaseUri = new Uri(apiUrl)
+            };
+            return client;
+        }
+
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
